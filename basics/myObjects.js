7a598e38e5c0fd9bf246c45688b6cc01dda4c83c
chris <mr.chrisnash@gmail.com>
2023-07-07 10:33:22 -0700

    more on types. readonly, optional chaining, combining with intersection operator

--- basics/myObjects.js
+++ basics/myObjects.js
@@ -12,13 +12,30 @@ function createUser(user) {
     console.log("User '".concat(user.name, "' has logged in with email '").concat(user.email, "', active status '").concat(user.isActive, "', and phone number '").concat(user.phoneNumber || "N/A", "'"));
 }
 createUser({
+    _id: "123",
     name: "Joe",
     email: "j@j.com",
     isActive: false,
 });
 createUser({
+    _id: "456",
     name: "Ben",
     email: "b@b.com",
     isActive: true,
     phoneNumber: "123-456-7890",
 });
+var myUser = {
+    _id: "742",
+    name: "Billy",
+    email: "billy@b.com",
+    isActive: true,
+};
+myUser.name = "Billy Bob";
+// myUser._id = '111'; // cannot change _id because it is readonly
+console.log(myUser._id);
+console.log(myUser === null || myUser === void 0 ? void 0 : myUser.phoneNumber); // optional chaining
+var card = {
+    cardNumber: '1234 5678 9012 3456',
+    cardDate: '12/23',
+    cvv: 123
+};
--- basics/myObjects.ts
+++ basics/myObjects.ts
@@ -12,6 +12,7 @@ console.log(course);
 //////////////////////////////
 
 type User = {
+  readonly _id: string;
   name: string;
   email: string;
   isActive: boolean;
@@ -25,19 +26,52 @@ function createUser(user: User): void {
 }
 
 createUser({
+  _id: "123",
   name: "Joe",
   email: "j@j.com",
   isActive: false,
 });
 
 createUser({
+  _id: "456",
   name: "Ben",
   email: "b@b.com",
   isActive: true,
   phoneNumber: "123-456-7890",
 });
 
+
+let myUser: User = {
+  _id: "742",
+  name: "Billy",
+  email: "billy@b.com",
+  isActive: true,
+};
+
+myUser.name = "Billy Bob";
+// myUser._id = '111'; // cannot change _id because it is readonly
+console.log(myUser._id);
+console.log(myUser?.phoneNumber); // optional chaining
+
 //////////////////////////////
+
+type cardNumber = {
+  cardNumber: string;
+}
+
+type cardDate = {
+  cardDate: string;
+}
+
+type cardDetails = cardNumber & cardDate & {
+  cvv: number
+}
+
+const card: cardDetails = {
+  cardNumber: '1234 5678 9012 3456',
+  cardDate: '12/23',
+  cvv: 123
+}
 
 
 
